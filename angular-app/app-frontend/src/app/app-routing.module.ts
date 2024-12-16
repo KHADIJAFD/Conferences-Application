77a82bcb69a5f1f3bd80a74468a615d18599a23e
KHADIJAFD <108407131+KHADIJAFD@users.noreply.github.com>
2024-12-16 18:06:22 +0100

    commit5

--- angular-app/app-frontend/src/app/app-routing.module.ts
+++ angular-app/app-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {KeynotesComponent} from "./ui/keynotes/keynotes.component";
+import {ConferencesComponent} from "./ui/conference/conference.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'conferences', component: ConferencesComponent},
+  { path: 'keynotes', component: KeynotesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
